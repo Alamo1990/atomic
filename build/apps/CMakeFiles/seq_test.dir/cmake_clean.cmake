f47a08db806cb0d900fbde4d6c0b0ddc67695501
alamo1990 <1990alamo@gmail.com>
2016-11-29 13:47:59 +0100

    compilado y runeado el sequencial para sacar las soluciones

---
+++ build/apps/CMakeFiles/seq_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seq_test.dir/seq_app.cpp.o"
+  "../seq_test.pdb"
+  "../seq_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seq_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
