5c9fa3f7ec08f6fbcefad5e893c7ea339d419f2f
Daniel Fernandez Rico <deferrico@gmail.com>
2016-12-09 16:39:01 +0100

    lock adv uses namespace std

--- apps/lock_app_adv.cpp
+++ apps/lock_app_adv.cpp
@@ -11,11 +11,11 @@
 #define NTHREADS 4
 constexpr int ImageHeight = 36;
 constexpr int ImageWidth = 36;
+using namespace std;
+mutex mtx;
 
-std::mutex mtx;
-
-std::pair<int,std::vector<std::vector<unsigned char> > > mandelbrot(double MaxRe, double MinRe, double MinIm, int order){
-        std::vector<std::vector<unsigned char> > image(ImageHeight, std::vector<unsigned char>(ImageWidth,0));
+pair<int,vector<vector<unsigned char> > > mandelbrot(double MaxRe, double MinRe, double MinIm, int order){
+        vector<vector<unsigned char> > image(ImageHeight, vector<unsigned char>(ImageWidth,0));
 //   double MinRe = -2.0;
 //   double MaxRe = p;
 //   double MinIm = -1.2;
@@ -46,15 +46,15 @@ std::pair<int,std::vector<std::vector<unsigned char> > > mandelbrot(double MaxRe
                         if(isInside) { image[x][y] = 255; }
                 }
         }
-        return std::make_pair(order,image);
+        return make_pair(order,image);
 }
 
-std::pair<int,std::vector<std::vector<std::complex<double> > > > FFT( std::pair<int,std::vector<std::vector<unsigned char> > > & image ){
-        std::vector<std::vector<std::complex<double> > > F(ImageHeight, std::vector<std::complex<double> >(ImageWidth));
-        std::complex<double> J (0,1);
+pair<int,vector<vector<complex<double> > > > FFT( pair<int,vector<vector<unsigned char> > > & image ){
+        vector<vector<complex<double> > > F(ImageHeight, vector<complex<double> >(ImageWidth));
+        complex<double> J (0,1);
         for(int k = 0; k< ImageHeight; k++) {
                 for(int l=0; l<ImageWidth; ++l) {
-                        std::complex<double> sum = 0;
+                        complex<double> sum = 0;
                         for(int m = 0; m< ImageHeight; m++) {
                                 for(int n=0; n<ImageWidth; ++n) {
                                         sum += (image.second[m][n]*1.0) * exp(-1.0*J*2.0*M_PI*((k*m*1.0)/(ImageHeight*1.0) + (l*n)/(ImageWidth*1.0)   ));
@@ -64,17 +64,17 @@ std::pair<int,std::vector<std::vector<std::complex<double> > > > FFT( std::pair<
                         F[k][l] = 1.0/(ImageHeight*ImageWidth) * sum;
                 }
         }
-        return std::make_pair(image.first,F);
+        return make_pair(image.first,F);
 
 }
 
 
-std::pair<int,std::vector<std::vector<unsigned char> > > IFFT(std::pair<int, std::vector<std::vector<std::complex<double> > > > & image){
-        std::vector<std::vector<unsigned char > > f(ImageHeight, std::vector<unsigned char>(ImageWidth));
-        std::complex<double> J (0,1);
+pair<int,vector<vector<unsigned char> > > IFFT(pair<int, vector<vector<complex<double> > > > & image){
+        vector<vector<unsigned char > > f(ImageHeight, vector<unsigned char>(ImageWidth));
+        complex<double> J (0,1);
         for(int m = 0; m< ImageHeight; ++m) {
                 for(int n=0; n<ImageWidth; ++n) {
-                        std::complex<double> sum = 0;
+                        complex<double> sum = 0;
                         for(int k = 0; k< ImageHeight; ++k) {
                                 for(int l=0; l<ImageWidth; ++l) {
                                         sum += (image.second[k][l]) * exp(J*2.0*M_PI*((k*m)/(ImageHeight*1.0) + (l*n)/(ImageWidth*1.0) ));
@@ -84,15 +84,15 @@ std::pair<int,std::vector<std::vector<unsigned char> > > IFFT(std::pair<int, std
                 }
         }
 
-        return std::make_pair(image.first, f);
+        return make_pair(image.first, f);
 }
 
-std::pair<int, std::vector< std::vector< std::complex<double> > > > Blur( std::pair<int, std::vector<std::vector<std::complex<double> > > > & image){
-        std::vector<std::vector<std::complex<double> > > F(ImageHeight, std::vector<std::complex<double> >(ImageWidth, std::complex<double>(0,0)));
+pair<int, vector< vector< complex<double> > > > Blur( pair<int, vector<vector<complex<double> > > > & image){
+        vector<vector<complex<double> > > F(ImageHeight, vector<complex<double> >(ImageWidth, complex<double>(0,0)));
         constexpr double kernel[3][3] = {{1/18.0, 1/18.0, 1/18.0},{1/18.0, 10/18.0, 1/18.0}, {1/18.0, 1/18.0, 1/18.0} };
         for(int y=1; y<ImageHeight-1; y++) {
                 for(int x=1; x<ImageWidth-1; x++) {
-                        std::complex<double> sum (0,0);
+                        complex<double> sum (0,0);
                         for(int k=-1; k<= 1; k++) {
                                 for(int j = -1; j<=1; j++) {
                                         sum += kernel[j+1][k+1] * image.second[y-j][x-k];
@@ -101,13 +101,13 @@ std::pair<int, std::vector< std::vector< std::complex<double> > > > Blur( std::p
                         F[y][x] = sum;
                 }
         }
-        return std::make_pair(image.first,F);
+        return make_pair(image.first,F);
 }
 
 
-void print(std::pair<int, std::vector<std::vector<unsigned char> > > image){
-        std::ofstream file;
-        file.open("output/"+std::to_string(image.first)+".txt");
+void print(pair<int, vector<vector<unsigned char> > > image){
+        ofstream file;
+        file.open("output/"+to_string(image.first)+".txt");
         for(int i = 0; i<ImageHeight; i++) {
                 for(int j=0; j<ImageWidth; j++) {
                         file<<(int)image.second[i][j];
@@ -117,26 +117,26 @@ void print(std::pair<int, std::vector<std::vector<unsigned char> > > image){
         file.close();
 }
 
-void scheduler(int nthreads, int nitems, locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >* queue0,
-               std::vector< std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > > > > & queues1){
-        std::cout << "schestart" << std::endl;
+void scheduler(int nthreads, int nitems, locked_buffer<pair<int,vector<vector<unsigned char> > > >* queue0,
+               vector< unique_ptr< locked_buffer<pair<int,vector<vector<unsigned char> > > > > > & queues1){
+        cout << "schestart" << endl;
         for (int i = 0; i < nitems; i++) {
-                std::cout << "schloopin: "<<i << std::endl;
+                cout << "schloopin: "<<i << endl;
                 int turn = i%nthreads;
-                std::cout << "schget: "<< i << std::endl;
-                auto image = std::get<1>(queue0->get());
-                std::cout << "schpreput: "<< i << std::endl;
+                cout << "schget: "<< i << endl;
+                auto image = get<1>(queue0->get());
+                cout << "schpreput: "<< i << endl;
                 queues1[turn]->put(image, i==(nitems-1));
         }
 }
 
 void looper(int mode,
             int nitems,
-            locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >* queue0,
-            std::unique_ptr<locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > > >& queue1,
-            std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<std::complex<double> > > > > >& queue2,
-            std::unique_ptr< locked_buffer<std::pair<int, std::vector< std::vector< std::complex<double> > > > > >& queue3,
-            locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >* queue4
+            locked_buffer<pair<int,vector<vector<unsigned char> > > >* queue0,
+            unique_ptr<locked_buffer<pair<int,vector<vector<unsigned char> > > > >& queue1,
+            unique_ptr< locked_buffer<pair<int,vector<vector<complex<double> > > > > >& queue2,
+            unique_ptr< locked_buffer<pair<int, vector< vector< complex<double> > > > > >& queue3,
+            locked_buffer<pair<int,vector<vector<unsigned char> > > >* queue4
             ){
         enum exec { M, F, B, I, P };
         bool last = false;
@@ -148,34 +148,34 @@ void looper(int mode,
 
                 switch (mode) {
                 case M:
-                        std::cout << "prebrot" << std::endl;
+                        cout << "prebrot" << endl;
                         queue0->put(mandelbrot(MaxRe, MinRe, MinIm, i), last); // no se si true o false
-                        std::cout << "postbrot" << std::endl;
+                        cout << "postbrot" << endl;
                         break;
                 case F: {
-                        std::cout << "getFFT" << std::endl;
-                        auto image = std::get<1>(queue1->get());
-                        std::cout << "preFFT" << std::endl;
+                        cout << "getFFT" << endl;
+                        auto image = get<1>(queue1->get());
+                        cout << "preFFT" << endl;
                         queue2->put(FFT(image), last);
-                        std::cout << "postFFT" << std::endl;
+                        cout << "postFFT" << endl;
                 }
                 break;
                 case B: {
-                        auto image = std::get<1>(queue2->get());
-                        std::cout << "preblur" << std::endl;
+                        auto image = get<1>(queue2->get());
+                        cout << "preblur" << endl;
                         queue3->put(Blur(image), last);
-                        std::cout << "postblur" << std::endl;
+                        cout << "postblur" << endl;
                         break;
                 }
                 case I: {
-                        auto image = std::get<1>(queue3->get());
-                        std::cout << "preinv" << std::endl;
+                        auto image = get<1>(queue3->get());
+                        cout << "preinv" << endl;
                         queue4->put(IFFT(image), last);
-                        std::cout << "postinv" << std::endl;
+                        cout << "postinv" << endl;
                 } break;
                 case P: {
                         mtx.lock();
-                        auto image = std::get<1>(queue4->get());
+                        auto image = get<1>(queue4->get());
                         mtx.unlock();
                         print(image);
                         break;
@@ -185,45 +185,45 @@ void looper(int mode,
 }
 int main(int argc, char* argv[]){
         if(argc != 4) {
-                std::cerr<<"Wrong arguments"<<std::endl;
-                std::cerr<<"Valid formats: "<<std::endl;
-                std::cerr<< " " << argv[0] << "nitems" << std::endl;
+                cerr<<"Wrong arguments"<<endl;
+                cerr<<"Valid formats: "<<endl;
+                cerr<< " " << argv[0] << "nitems" << endl;
         }
-        const long nitems = std::stol(argv[1]);
-        const int buff_size = std::stoi(argv[2]);
-        const int nthreads = std::stoi(argv[3]);
+        const long nitems = stol(argv[1]);
+        const int buff_size = stoi(argv[2]);
+        const int nthreads = stoi(argv[3]);
 
 
-        locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >* queue0 = new locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >(buff_size);
+        locked_buffer<pair<int,vector<vector<unsigned char> > > >* queue0 = new locked_buffer<pair<int,vector<vector<unsigned char> > > >(buff_size);
 
-        std::vector< std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > > > >  queues1;
-        std::vector< std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<std::complex<double> > > > > > >  queues2;
-        std::vector< std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<std::complex<double> > > > > > >  queues3;
+        vector< unique_ptr< locked_buffer<pair<int,vector<vector<unsigned char> > > > > >  queues1;
+        vector< unique_ptr< locked_buffer<pair<int,vector<vector<complex<double> > > > > > >  queues2;
+        vector< unique_ptr< locked_buffer<pair<int,vector<vector<complex<double> > > > > > >  queues3;
         for(int i=0; i<nthreads; i++) {
-                queues1.push_back( std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > > > (new locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >(buff_size)));
-                queues2.push_back( std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<std::complex<double> > > > > > (new locked_buffer<std::pair<int,std::vector<std::vector<std::complex<double> > > > >(buff_size)));
-                queues3.push_back( std::unique_ptr< locked_buffer<std::pair<int,std::vector<std::vector<std::complex<double> > > > > > (new locked_buffer<std::pair<int,std::vector<std::vector<std::complex<double> > > > >(buff_size)));
+                queues1.push_back( unique_ptr< locked_buffer<pair<int,vector<vector<unsigned char> > > > > (new locked_buffer<pair<int,vector<vector<unsigned char> > > >(buff_size)));
+                queues2.push_back( unique_ptr< locked_buffer<pair<int,vector<vector<complex<double> > > > > > (new locked_buffer<pair<int,vector<vector<complex<double> > > > >(buff_size)));
+                queues3.push_back( unique_ptr< locked_buffer<pair<int,vector<vector<complex<double> > > > > > (new locked_buffer<pair<int,vector<vector<complex<double> > > > >(buff_size)));
         }
-        locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >* queue4 = new locked_buffer<std::pair<int,std::vector<std::vector<unsigned char> > > >(buff_size);
+        locked_buffer<pair<int,vector<vector<unsigned char> > > >* queue4 = new locked_buffer<pair<int,vector<vector<unsigned char> > > >(buff_size);
 
 
-        std::vector<std::thread> threads(2+nthreads*3+1);
+        vector<thread> threads(2+nthreads*3+1);
 
-        threads.at(0) = std::thread(looper, 0, nitems, queue0, ref(queues1[0]), ref(queues2[0]), ref(queues3[0]), queue4);
-        std::cout << "0" << std::endl;
-        threads.at(1) = std::thread(scheduler, nthreads, nitems, queue0, ref(queues1));
-        std::cout << "1" << std::endl;
+        threads.at(0) = thread(looper, 0, nitems, queue0, ref(queues1[0]), ref(queues2[0]), ref(queues3[0]), queue4);
+        cout << "0" << endl;
+        threads.at(1) = thread(scheduler, nthreads, nitems, queue0, ref(queues1));
+        cout << "1" << endl;
         for (int i = 0; i < nthreads; i++) {
-                std::cout << "loopstart: "<< i << std::endl;
-                threads.at(3+i*3) = std::thread(looper, 1, nitems, queue0, ref(queues1[i]), ref(queues2[i]), ref(queues3[i]), queue4);
-                std::cout << "2: "<< i << std::endl;
-                threads.at(4+i*3) = std::thread(looper, 2, nitems, queue0, ref(queues1[i]), ref(queues2[i]), ref(queues3[i]), queue4);
-                std::cout << "3: "<<i << std::endl;
-                threads.at(5+i*3) = std::thread(looper, 3, nitems, queue0, ref(queues1[i]), ref(queues2[i]), ref(queues3[i]), queue4);
-                std::cout << "4:"<< i << std::endl;
+                cout << "loopstart: "<< i << endl;
+                threads.at(3+i*3) = thread(looper, 1, nitems, queue0, ref(queues1[i]), ref(queues2[i]), ref(queues3[i]), queue4);
+                cout << "2: "<< i << endl;
+                threads.at(4+i*3) = thread(looper, 2, nitems, queue0, ref(queues1[i]), ref(queues2[i]), ref(queues3[i]), queue4);
+                cout << "3: "<<i << endl;
+                threads.at(5+i*3) = thread(looper, 3, nitems, queue0, ref(queues1[i]), ref(queues2[i]), ref(queues3[i]), queue4);
+                cout << "4:"<< i << endl;
         }
-        threads.at(2+nthreads*3) = std::thread(looper, 4,nitems, queue0, ref(queues1[0]), ref(queues2[0]), ref(queues3[0]), queue4);
-        std::cout << "5" << std::endl;
+        threads.at(2+nthreads*3) = thread(looper, 4,nitems, queue0, ref(queues1[0]), ref(queues2[0]), ref(queues3[0]), queue4);
+        cout << "5" << endl;
 
         for(auto& th : threads) th.join();
 
